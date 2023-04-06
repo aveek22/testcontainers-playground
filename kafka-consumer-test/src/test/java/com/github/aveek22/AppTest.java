3d271e06317c225a879d06c6d6576aa7f7cbb1cb
Aveek Das <aveek.das@dpgmedia.be>
2023-04-06 23:05:31 +0200

    Add test containers

--- kafka-consumer-test/src/test/java/com/github/aveek22/AppTest.java
+++ kafka-consumer-test/src/test/java/com/github/aveek22/AppTest.java
@@ -3,36 +3,50 @@ package com.github.aveek22;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+@Testcontainers
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
+
+
+
+    @Container
+    KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.0"));
+//            .withExposedPorts(9092);
+
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigorous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        kafka.start();
+//        kafka.getBootstrapServers();
+        System.out.println(kafka.getBootstrapServers());
+
+        assertTrue(true);
     }
 }
